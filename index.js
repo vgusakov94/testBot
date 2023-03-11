a6ac357b38ce61168b34d56e1bee6ff4bc8d43f9
Vitaly Gusakov <61951373+vgusakov94@users.noreply.github.com>
2023-03-11 15:55:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const port = process.env.PORT || 443;
 const host = "0.0.0.0";
 const externalUrl = process.env.CUSTOM_ENV_VARIABLE || "https://web-production-4d31.up.railway.app/";
-const token = '1715459031:AAGWHxzh6wA_iL4eqq_XsHus3cPnKybY6To';
+const token = '5993412829:AAEMCxk_2MplGufZFb7Tog1CUXr3OYpgYIY';
 const bot = new TelegramBot(token, {webHook: {port: port, host: host}});
 bot.setWebHook(externalUrl + ':443/bot' + token);
 
